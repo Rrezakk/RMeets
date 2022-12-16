87089dacfba68c40d63c119b6824ea8c2566941f
Rrezakk <52713544+Rrezakk@users.noreply.github.com>
2022-12-16 12:30:11 +0300

    Index

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,21 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using RMeets.Contexts;
-
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 namespace RMeets.Pages;
-
 public class IndexModel : PageModel
 {
-    private readonly ILogger<IndexModel> _logger;
-    private readonly ApplicationContext _context;
-
-    public IndexModel(ILogger<IndexModel> logger,ApplicationContext context)
-    {
-        _logger = logger;
-        _context = context;
-    }
-
-    public void OnGet()
+    public IndexModel()
     {
         
     }
